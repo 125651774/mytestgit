e223f41d8a141d4450362cc5982e09becfba3b7f
taoliang <125651774@qq.com>
2018-12-17 08:29:15 +0800

    测试

---
+++ src/main/test/java/test2.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: test2
+ * Author:   liang.tao
+ * Date:     2018/12/17 8:23
+ * Description: 测试2
+ * History:
+ */
+
+/**
+ * 〈功能简述〉 
+ * 〈测试2〉
+ *
+ * @author liang.tao
+ * @create 2018/12/17
+ * @since 1.0.0
+ */
+public class test2 {
+    String a = "测试";
+}
