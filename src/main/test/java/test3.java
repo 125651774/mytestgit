7b057c88d980e95544c54c38dc6a9a05d9ff56db
taoliang <125651774@qq.com>
2018-12-17 09:40:20 +0800

    添加test3

---
+++ src/main/test/java/test3.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: test3
+ * Author:   liang.tao
+ * Date:     2018/12/17 9:37
+ * Description: 测试3
+ * History:
+ */
+
+/**
+ * 〈功能简述〉 
+ * 〈测试3〉
+ *
+ * @author liang.tao
+ * @create 2018/12/17
+ * @since 1.0.0
+ */
+public class test3 {
+
+}
