b7c4972bbe9f5dfdd8432e9787b3658c358b7bd8
taoliang <125651774@qq.com>
2018-12-17 09:41:28 +0800

    添加testdao

---
+++ src/main/java/com/lxk/dao/testdao.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: testdao
+ * Author:   liang.tao
+ * Date:     2018/12/17 9:39
+ * Description: 测试dao
+ * History:
+ */
+package com.lxk.dao;
+
+/**
+ * 〈功能简述〉 
+ * 〈测试dao〉
+ *
+ * @author liang.tao
+ * @create 2018/12/17
+ * @since 1.0.0
+ */
+public class testdao {
+
+}
