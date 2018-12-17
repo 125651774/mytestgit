fd358bd7bb55148845377d3ea658237d130c4b53
taoliang <125651774@qq.com>
2018-12-17 09:04:11 +0800

    测试

--- src/main/test/java/test.java
+++ src/main/test/java/test.java
@@ -22,6 +22,6 @@ public class test {
         System.out.println("-----------print-----------值="+a +","+"当前类:test.()");
         System.out.println("-----------print-----------值="+a +","+"当前类:test.()");
         System.out.println("-----------print-----------值="+a +","+"当前类:test.()");
-        System.out.println("测试git");
+        System.out.println("测试git11111111111111111");
     }
 }
