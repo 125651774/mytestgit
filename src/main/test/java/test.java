fcf7717158509bd7145459c967d432169c777b42
taoliang <125651774@qq.com>
2018-12-17 08:34:39 +0800

    测试

---
+++ src/main/test/java/test.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: test
+ * Author:   liang.tao
+ * Date:     2018/12/13 16:24
+ * Description: 测试
+ * History:
+ */
+
+/**
+ * 〈功能简述〉 
+ * 〈测试〉
+ *
+ * @author liang.tao
+ * @create 2018/12/13
+ * @since 1.0.0
+ */
+public class test {
+    public static void  main(String[] args){
+        System.out.println("-----------print1111111111-----------值="+ +","+"当前类:test.()");
+        System.out.println("-----------print-----------值="+ +","+"当前类:test.()");
+        System.out.println("-----------print-----------值="+ +","+"当前类:test.()");
+        System.out.println("-----------print-----------值="+ +","+"当前类:test.()");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/Display.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liang.tao
+  Date: 2018/12/13
+  Time: 14:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h2>Hello World!</h2>
+<h2>这是自己添加的文字</h2>
+</body>
+</html>
